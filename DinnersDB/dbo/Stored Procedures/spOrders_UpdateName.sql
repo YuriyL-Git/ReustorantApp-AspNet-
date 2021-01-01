4026e5fac0362a4a8b5419eea233839d0ede3a69
Yuriy <xaosmmm@gmail.com>
2021-01-01 12:21:55 +0200

    Created Data Base and Stored procedures

---
+++ DinnersDB/dbo/Stored Procedures/spOrders_UpdateName.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spOrders_UpdateName]
+@Id int,
+@OrderName nvarchar(50)
+AS
+
+begin
+set nocount on;
+
+update dbo.[Order]
+SET OrderName = @OrderName
+where Id = @Id;
+
+
+end
+
